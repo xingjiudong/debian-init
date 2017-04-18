31db33859107ea174eb0fcd99434bf2980dda06b
thinkernel <thinkernel@gmail.com>
2017-04-18 17:42:55 +0800

    Config docker with etcd cluster support

--- config-docker-service.sh
+++ config-docker-service.sh
@@ -1,8 +1,23 @@
 #!/bin/bash
 set -e
 
+IPV4_ADDR=${IPV4_ADDR:-$1}
+ETCD_DISCOVERY=${ETCD_DISCOVERY:-$2}
+
+# Setup and copy etcd environment file
+sed "s#{{PUBLIC_ADDR}}#$IPV4_ADDR#g" etcd2.env > /etc/etcd2.env
+sed -i "s#{{ETCD_DISCOVERY_URL}}#$ETCD_DISCOVERY#g" /etc/etcd2.env
+# Copy etcd service file
+cp etcd2.service /etc/systemd/system/etcd2.service
+
+# Add DOCKER_OPTS into docker.service file
 sed -i "s#ExecStart=/usr/bin/dockerd#ExecStart=/usr/bin/dockerd \$DOCKER_OPTS#" /lib/systemd/system/docker.service
 
+# Copy docker service dropin file
 DOCKER_CONF_PATH=/etc/systemd/system/docker.service.d
 mkdir -p ${DOCKER_CONF_PATH}
 cp 20-docker.conf ${DOCKER_CONF_PATH}/20-docker.conf
+cp 10-wait-docker.conf ${DOCKER_CONF_PATH}/10-wait-docker.conf
+
+# Open 2375 port for docker service
+cp docker-tcp.socket /etc/systemd/system/docker-tcp.socket
